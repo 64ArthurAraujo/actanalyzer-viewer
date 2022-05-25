d44ac73f5428ac4b765bccbdaf43df40b113ce3e
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-05-25 18:43:28 -0300

    Add exception handling

--- src/api/actanalyzer-wrapper.ts
+++ src/api/actanalyzer-wrapper.ts
@@ -3,22 +3,42 @@ import axios from "axios"
 const client = axios.create({ baseURL: "http://localhost:8080/api/v0.0.1" })
 
 export async function getAllUsers() {
-    const { data } = await client.get("/retrieve/users/")
-    return data
+    try {
+        const { data } = await client.get("/retrieve/users/")
+        return data
+        
+    } catch (Exception) {
+        console.error(Exception);
+    }
 }
 
 export async function getTotalUserSearches(id: string) {
-    const { data } = await client.get(`/retrieve/users/${id}/searches/total`)
-    return data
+    try {
+        const { data } = await client.get(`/retrieve/users/${id}/searches/total`)
+        return data
+        
+    } catch (Exception) {
+        console.error(Exception);
+    }
 }
 
 export async function getCategorisedUserSearches(id: string) {
-    const { data } = await client.get(`/retrieve/users/${id}/searches/categorised`)
-    return data
+    try {
+        const { data } = await client.get(`/retrieve/users/${id}/searches/categorised`)
+        return data
+
+    } catch (Exception) {
+        console.error(Exception);
+    }
 }
 
 export async function getUser(id: string) {
-    const { data } = await client.get(`/retrieve/users/${id}`)
-    return data
+    try {
+        const { data } = await client.get(`/retrieve/users/${id}`)
+        return data
+
+    } catch (Exception) {
+        console.error(Exception);
+    }
 }
 
