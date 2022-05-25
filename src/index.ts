87653a63851c123eeb112b14ea99d19838361509
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-05-25 10:48:26 -0300

    Set view engine and render pages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express, { Express, Request, Response } from 'express'
+import hbs from 'hbs'
+
+import { getTranslationsFor } from './lang'
+import { getAllUsers } from './api'
 
 const server: Express = express()
 // Do not run on 8080 because its the actanalyzer's port
 const port = 8081
 
+server.set('view engine', 'hbs')
+server.set('views', __dirname + "/views")
+
+
 server.get('/', (request: Request, response: Response) => {
-    response.sendFile(__dirname + "/pages/index.html");
+    const browserLanguage = request.acceptsLanguages()[1]
+
+    response.render('index.hbs', { translation: getTranslationsFor(browserLanguage) })
+})
+
+server.get('/users/list', async (request: Request, response: Response) => {
+    const browserLanguage = request.acceptsLanguages()[1]
+
+    response.render('users-list.hbs', { 
+        translation: getTranslationsFor(browserLanguage),
+        users: await getAllUsers()
+    })
 })
 
 server.listen(port, () => console.log(`Running on http://localhost:${port}/`))
