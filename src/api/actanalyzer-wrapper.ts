bf5c01f71412bfd60c71e4a53bcd1edcdfb9ea86
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-05-25 11:02:10 -0300

    Retrieve user total searches from the API

--- src/api/actanalyzer-wrapper.ts
+++ src/api/actanalyzer-wrapper.ts
@@ -4,7 +4,10 @@ const client = axios.create({ baseURL: "http://localhost:8080/api/v0.0.1" })
 
 export async function getAllUsers() {
     const { data } = await client.get("/retrieve/users/")
-
     return data
 }
 
+export async function getTotalUserSearches(id: string) {
+    const { data } = await client.get(`/retrieve/users/${id}/searches/total`)
+    return data
+}
