a6f3ad0aca9c6c85bfa9b3c83c31d07675f1ac05
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-05-27 05:44:22 -0300

    Fix translation

--- src/lang/lang-chooser.ts
+++ src/lang/lang-chooser.ts
@@ -3,7 +3,7 @@ import portuguese from './json/pt.json'
 
 export function getTranslationsFor(language: String) {
     switch (language) {
-        case 'en': return english
         case 'pt': return portuguese
+        default: case 'en': return english
     }
 }
